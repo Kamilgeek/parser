9035e3a4e9521c763415221b3848c1b4409652b3
Kamilgeek <kamil-magomedov-00@mail.ru>
2020-11-05 16:08:50 +0400

    did

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import requests
 from bs4 import BeautifulSoup as BS
 
-r = requests.get('https://stopgame.ru/review/new/stopchoice')
-html = BS(r.content, 'html.parser')
+max_page = 4
+pages = []
 
-for el in html.select('.article-summary'):
-    title = el.select('.caption-bold > a')
-    print( title[0].text )
+for x in range(1, max_page + 1):
+    pages.append(requests.get('https://stopgame.ru/review/new/stopchoice/p' + str(x)))
+for r in pages:
+    html = BS(r.content, 'html.parser')
+
+    for el in html.select('.article-summary'):
+        title = el.select('.caption-bold > a')
+        print( title[0].text )
